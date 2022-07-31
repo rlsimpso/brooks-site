685d44411673521014cf47b488b480caf2329a81
Ryan Simpson <rlsimpso91@gmail.com>
2022-07-31 09:24:14 -0500

    First commit

---
+++ index.js
@@ -0,0 +1,76 @@
+const path = require('path')
+const expressEdge = require('express-edge')
+const express = require('express')
+const edge = require('edge.js')
+const mongoose = require('mongoose')
+const bodyParser = require('body-parser')
+const fileUpload = require('express-fileupload')
+const expressSession = require('express-session')
+const connectMongo = require('connect-mongo')
+const connectFlash = require('connect-flash')
+
+const createPostController = require('./controllers/createPost')
+const homePageController = require('./controllers/homePage')
+const storePostController = require('./controllers/storePost')
+const getPostController = require('./controllers/getPost')
+const createUserController = require('./controllers/createUser')
+const storeUserController = require('./controllers/storeUser')
+const loginController = require("./controllers/login")
+const loginUserController = require('./controllers/loginUser')
+const logoutController = require("./controllers/logout")
+
+
+const app = new express()
+
+mongoose.connect('mongodb://localhost:27017/brooks-blog', { useNewUrlParser: true, useUnifiedTopology: true})
+    .then(() => 'You are connected to Mongo')
+    .catch(err => console.error('Something went wrong', err))
+
+const mongoStore = connectMongo(expressSession)
+
+app.use(expressSession({
+    secret: 'secret',
+    store: new mongoStore({
+        mongooseConnection: mongoose.connection
+    })
+}))
+
+app.use(fileUpload())
+app.use(express.static('public'))
+app.use(expressEdge.engine)
+app.set('views', __dirname + '/views')
+
+app.use('*', (req, res, next) => {
+    edge.global('auth', req.session.userId)
+    next()
+})
+
+app.use(bodyParser.urlencoded({ extended: true }))
+app.use(connectFlash())
+
+const storePost = require('./middleware/storePost')
+const auth = require("./middleware/auth")
+const redirectIfAuthenticated = require('./middleware/redirectIfAuthenticated')
+app.use('/posts/store', storePost)
+
+app.get("/", homePageController)
+app.get("/post/:id", getPostController)
+app.get("/posts/new", auth, createPostController)
+app.post("/posts/store", auth, storePostController)
+app.get("/auth/register", redirectIfAuthenticated, createUserController)
+app.get('/auth/login', redirectIfAuthenticated, loginController)
+app.post("/users/register", redirectIfAuthenticated, storeUserController)
+app.post("/users/login", redirectIfAuthenticated, loginUserController)
+app.get("/auth/logout", redirectIfAuthenticated, logoutController)
+
+app.get('/about', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'pages/about.html'))
+})
+
+app.get('/contact', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'pages/contact.html'))
+})
+
+app.listen(4000, ()=> {
+    console.log('App is listening on port 4000');
+})
